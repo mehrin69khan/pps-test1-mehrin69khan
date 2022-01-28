2a3d60c2ab079b949e5de65ed2283c5aeba0b42a
Mehrin Khan <95270198+mehrin69khan@users.noreply.github.com>
2022-01-28 18:31:00 +0000

    p1debuglog.txt completed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,14 @@
-
+#include <stdio.h>
+void add()
+{
+  int a,b,c;
+  printf("enter any two numbers:\n");
+  scanf ("%d%d",&a,&b);
+  c=a+b;
+  printf("addition of two numbers:%d\n",c);
+}
+int main()
+{
+  add();
+  return 0;
 }
